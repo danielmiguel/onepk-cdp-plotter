ba8cb5d71d88991ac5c5293c8818300485acc282
Evelio VILA <eveliovila@gmail.com>
2014-07-13 00:45:33 +0200

    change to a class struct

--- session.py
+++ session.py
@@ -12,23 +12,40 @@ from onep.element import SessionProperty
 from onep.core.exception import OnepException
 from onep.core.util import tlspinning
 
-
 element_hostname = '10.0.2.17'
 username = 'evelio'
 password = 'vila'
 transport = "tls"
 network_element = None
 session_handle = None
-root_cert_path = 'ca.pem'
+root_cert_path = './ca.pem'
 aplicationName = 'cdp-plotter'
 
-network_application = NetworkApplication.get_instance()
-network_application.name = applicationName
-network_element = network_application.get_network_element(element_hostname)
+class Create_Ne(name,ca,ip):
 
-session_config = SessionConfig(SessionConfig.SessionTransportMode.TLS)
-session_config.ca_certs = root_cert_path
+    def __init__(self,name):
+        self.app_name = name
+        self.ca_path = ca
+        self.ip = ip
+            
+    def config(self):
+        self.session_config = SessionConfig(SessionConfig.SessionTransportMode.TLS)
+        self.session_config.ca_certs = self.ca_path
 
-session_handle = network_element.connect(username, password, session_config)
+    def get_ne(self,ip):
+        network_application = NetworkApplication.get_instance()
+        network_application.name = self.app_name
+        network_element = network_application.get_network_element(self.ip)
+        return network_element
+
+
+if __name__ == '__main__':
+
+    cdp-plotter = Create_Ne(aplicationName,root_cert_path)
+    ne = cdp_plotter.get_ne(element_hostname)
+    session_config = ne.config()
+    session_handle = network_element.connect(username, password, session_config)
+
+
 
 
