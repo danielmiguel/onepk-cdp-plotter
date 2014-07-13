a1d1a3f79816df5886c9e2d03c48090fc7091ccf
Evelio VILA <eveliovila@gmail.com>
2014-07-13 16:47:48 +0200

    import stuff

---
+++ topology.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from onep.topology import TopologyListener
+from onep.topology import TopologyEvent
+from onep.topology import TopologyClass
+from onep.topology import TopologyFilter
+from onep.topology import Edge
+import session
+
+
+
